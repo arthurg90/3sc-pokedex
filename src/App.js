84cf86c1b8b67fc32bd97d32b4443f4fd2a36330
Arthur Graham <agrahamwk@gmail.com>
2021-05-27 17:29:15 +0100

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import Header from "./components/Header";
-import DataComponent from './components/DataComponent';
+import PokeList from './components/PokeList';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([
+        {
+            "name": "bulbasaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/1/"
+        },
+        {
+            "name": "ivysaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/2/"
+        },
+        {
+            "name": "venusaur",
+            "url": "https://pokeapi.co/api/v2/pokemon/3/"
+        }
+      ]);   
+
   return (
     <div className="App">
       <Header text="Pokedex" />
-      <DataComponent />
+      <PokeList items={items} />
     </div>
   );
 }
