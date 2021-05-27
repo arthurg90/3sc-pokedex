400abf10fd8aa48f18ce574d6169de26920a9761
Arthur Graham <agrahamwk@gmail.com>
2021-05-27 21:01:16 +0100

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
 import Header from "./components/Header";
-import PokeList from './components/PokeList';
-import { useEffect, useState } from 'react';
+import PokeList from "./components/PokeList";
+import { useEffect, useState } from "react";
 
 function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]); 
+  const [items, setItems] = useState([]);
 
   const getPokemon = () => {
-    return fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
-      .then(res => res.json())
+    return fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
       .then(
         (name) => {
           setIsLoaded(true);
@@ -20,24 +20,24 @@ function App() {
           setIsLoaded(true);
           setError(error);
         }
-      )
-  } 
-  
+      );
+  };
+
   useEffect(() => {
     getPokemon();
-  }, []) 
+  }, []);
 
   if (error) {
-    return <div>Error: {error.message}</div>
+    return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   } else {
-  return (
-    <div className="App">
-      <Header text="Pokedex App" />
+    return (
+      <div className="App">
+        <Header text="Pokedex" />
         <PokeList items={items} />
-    </div>
-  );
+      </div>
+    );
   }
 }
 
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,17 +1,35 @@
+import styled from "styled-components";
+
+const StyledList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const ListItem = styled.div`
+  margin: 2rem;
+  width: 250px;
+`;
+
 const PokeList = (props) => {
   function capitalise(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
   return (
-    <>
-        {props.items.map((item) => (
-          <div key={item.name}>
-            <img alt="pokemon" width='auto' height='120px' src={`https://img.pokemondb.net/artwork/large/${item.name}.jpg`}/>
-            <h2>{capitalise(item.name)}</h2>
-          </div>
-        ))}
-    </>
-  )
-}
+    <StyledList>
+      {props.items.map((item) => (
+        <ListItem key={item.name}>
+          <img
+            alt="pokemon"
+            width="auto"
+            height="120px"
+            src={`https://img.pokemondb.net/artwork/large/${item.name}.jpg`}
+          />
+          <h2>{capitalise(item.name)}</h2>
+        </ListItem>
+      ))}
+    </StyledList>
+  );
+};
 
 export default PokeList;
