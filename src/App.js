d58b80cc1cfaaff509cc72e5d658d9983e647914
Arthur Graham <agrahamwk@gmail.com>
2021-05-29 18:37:23 +0100

    add generation selection options

--- src/App.js
+++ src/App.js
@@ -20,16 +20,17 @@ function App() {
   const [items, setItems] = useState([]);
   const [search, setSearch] = useState('');
   const [saved, setSaved] = useState([]);
+  const [generation, setGeneration] = useState(1);
 
   //Fetch API for all pokemon results
-  const getPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/?limit=151`;
+  const getPokemon = (generation) => {
+    const url = `https://pokeapi.co/api/v2/generation/${generation}/`;
     return fetch(url)
       .then((res) => res.json())
       .then(
         (name) => {
           setIsLoaded(true);
-          setItems(name.results);
+          setItems(name.pokemon_species)
         },
         (error) => {
           setIsLoaded(true);
@@ -39,8 +40,12 @@ function App() {
   };
 
   useEffect(() => {
-    getPokemon();
-  }, []);
+    getPokemon(generation);
+  }, [generation]);
+
+  function handleSelect(e) {
+    setGeneration(e.target.value);
+  }
 
   useEffect(() => {
     const savedPokemon = JSON.parse(
@@ -82,7 +87,24 @@ function App() {
       <ModalProvider>
         <div className="App">
           <Header />
+          <label htmlFor="gen-select">Choose a generation:</label>
+          <select
+            value={generation}
+            id="gen-select"
+            onChange={handleSelect}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+          </select>
+
           <Search handleChange={(e) => setSearch(e.target.value)} />
+
           {saved && saved.length > 0 ? (
             <>
               <StyledHeading>Saved:</StyledHeading>
@@ -93,7 +115,7 @@ function App() {
               />
             </>
           ) : null}
-          <StyledHeading>Kanto Pokemon:</StyledHeading>
+          <StyledHeading>Pokemon:</StyledHeading>
           <PokeList
             items={searchResults}
             saveComponent={SavePokemon}
